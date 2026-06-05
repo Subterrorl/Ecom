29c2ae2c219d242ac76c38729c9bb2d5dd131d26
Subterrorl <nutchanon.ta@gmail.com>
2026-06-06 01:44:38 +0700

    day13

--- client/src/components/admin/Sidebaradmin.jsx
+++ client/src/components/admin/Sidebaradmin.jsx
@@ -1,13 +1,17 @@
 //rafce
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { LayoutDashboard } from "lucide-react";
 
 const Sidebaradmin = () => {
   return (
     <div className="bg-gray-800 w-64 text-gray-100 flex flex-col h-screen">
       <div className="h-24 bg-gray-900 flex items-center justify-center text-2xl font-bold">Admin Panel</div>
       <nav className="flex-1 px-4 py-4">
-        <NavLink>Dashboard</NavLink>
+        <NavLink>
+          <LayoutDashboard />
+          Dashboard
+        </NavLink>
       </nav>
       <div>Footer</div>
     </div>
