228e9d9e6ce1527d094870d74d9e1359eed48639
Subterrorl <nutchanon.ta@gmail.com>
2026-06-17 17:27:19 +0700

    day20 search filter category + price

--- client/src/components/card/SearchCard.jsx
+++ client/src/components/card/SearchCard.jsx
@@ -1,25 +1,72 @@
 //rafce
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import useEcomStore from "../../store/ecom-store";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 const SearchCard = () => {
   const getProduct = useEcomStore((state) => state.getProduct);
   const products = useEcomStore((state) => state.products);
+
   const actionSearchFilters = useEcomStore(
     (state) => state.actionSearchFilters,
   );
+
+  const getCategory = useEcomStore((state) => state.getCategory);
+  const categories = useEcomStore((state) => state.categories);
+
   const [text, setText] = useState("");
-  console.log("text", text);
+  const [categorySelected, setCategorySelected] = useState([]);
+
+  const [price, setPrice] = useState([1000, 30000]);
+  const [ok, setOk] = useState(false);
+
+  useEffect(() => {
+    getCategory();
+  }, []);
 
   useEffect(() => {
     const delay = setTimeout(() => {
-      actionSearchFilters({ query: text });
-      if (!text) {
+      if (text) {
+        actionSearchFilters({ query: text });
+      } else {
         getProduct();
       }
     }, 300);
     return () => clearTimeout(delay);
   }, [text]);
+
+  const handleCheck = (e) => {
+    console.log(e.target.value);
+    const inCheck = e.target.value;
+    const inState = [...categorySelected];
+    const findCheck = inState.indexOf(inCheck);
+
+    if (findCheck === -1) {
+      inState.push(inCheck);
+    } else {
+      inState.splice(findCheck, 1);
+    }
+    setCategorySelected(inState);
+
+    if (inState > 0) {
+      actionSearchFilters({ category: inState });
+    } else {
+      getProduct();
+    }
+  };
+  console.log(categorySelected);
+
+  useEffect(() => {
+    actionSearchFilters({ price });
+  }, [ok]);
+  const handlePrice = (value) => {
+    console.log(value);
+    setPrice(value);
+    setTimeout(() => {
+      setOk(!ok);
+    }, 300);
+  };
   return (
     <div>
       <h1 className="text-xl font-bold mb-4">ค้นหาสินค้า</h1>
@@ -28,6 +75,35 @@ const SearchCard = () => {
         type="text"
         className="border border-gray-800 rounded-md w-full mb-4"
       />
+      <hr />
+      <div>
+        <h1>หมวดหมู่สินค้า</h1>
+        <div>
+          {categories.map((item, index) => (
+            <div className="flex gap-2">
+              <input type="checkbox" value={item.id} onChange={handleCheck} />
+              <label>{item.name}</label>
+            </div>
+          ))}
+        </div>
+      </div>
+      <hr />
+      <div>
+        <h1>ค้นหาราคา</h1>
+        <div>
+          <div className="flex justify-between">
+            <span>Min : {price[0]}</span>
+            <span>Max : {price[1]}</span>
+          </div>
+          <Slider
+            onChange={handlePrice}
+            range
+            min={0}
+            max={100000}
+            defaultValue={[1000, 30000]}
+          />
+        </div>
+      </div>
     </div>
   );
 };
