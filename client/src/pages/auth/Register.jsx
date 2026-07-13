78890cb11066ba995ca7ea2cdd80de2cc5429555
Subterrorl <nutchanon.ta@gmail.com>
2026-07-13 12:16:41 +0700

    day34 confirmpassword

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -2,8 +2,33 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
+import { useForm } from "react-hook-form";
+import zxcvbn from "zxcvbn";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
+
+const registerSchema = z
+  .object({
+    email: z.string().email({ message: "Invalid email" }),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters" }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password does not match",
+    path: ["confirmPassword"],
+  });
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(registerSchema),
+  });
+
   const [form, setForm] = useState({
     email: "",
     password: "",
@@ -17,7 +42,7 @@ const Register = () => {
       [e.target.name]: e.target.value,
     });
   };
-  const handleSubmit = async (e) => {
+  const hdlSubmit = async (e) => {
     e.preventDefault();
 
     if (form.password !== form.confirmPassword) {
@@ -35,16 +60,50 @@ const Register = () => {
     }
   };
 
+  const onSubmit = async (data) => {
+    const passwordScore = zxcvbn(data.password).score;
+    if (passwordScore < 3) {
+      toast.warning("Password strength is too weak");
+      return;
+    }
+
+    try {
+      const res = await axios.post("http://localhost:5000/api/register", data);
+      console.log(res.data);
+      toast.success(res.data.message);
+    } catch (err) {
+      const errMsG = err.response?.data?.error;
+      toast.error(errMsG);
+      console.log(err.response);
+    }
+  };
+
   return (
     <div>
       Register
-      <form onSubmit={handleSubmit}>
-        Email
-        <input className="border" onChange={handleOnChange} name="email" type="email" />
-        Password
-        <input className="border" onChange={handleOnChange} name="password" type="text" />
-        Confirm Password
-        <input className="border" onChange={handleOnChange} name="confirmPassword" type="text" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          Email
+          <input className="border" {...register("email")} />
+          {errors.email && (
+            <p className="text-red-500">{errors.email.message}</p>
+          )}
+        </div>
+
+        <div>
+          Password
+          <input className="border" {...register("password")} />
+          {errors.password && (
+            <p className="text-red-500">{errors.password.message}</p>
+          )}
+        </div>
+        <div>
+          Confirm Password
+          <input className="border" {...register("confirmPassword")} />
+          {errors.password && (
+            <p className="text-red-500">{errors.confirmPassword.message}</p>
+          )}
+        </div>
         <button className="bg-blue-500">Register</button>
       </form>
     </div>
